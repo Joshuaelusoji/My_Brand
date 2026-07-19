50bfa9facc14953f7c0743c313fd563d8fe93200
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-07-19 08:26:20 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div className="flex justify-between items-center px-8 py-4 md:fixed md:w-full text-white">
         <div className="flex items-center text-lg sm:text-xl md:text-2xl font-bold">
           <img src={BrandLogo} alt="Brand Logo" className="h-6 w-6 mr-2" />
-          Joshua<span className="text-yellow-500">Elusoji</span>
+          Joshua<span className="">Elusoji</span>
         </div>
         <button className="sm:hidden flex items-center focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
           <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -19,16 +19,16 @@ export default function Navbar() {
           </svg>
         </button>
         <ul className="hidden sm:flex items-center space-x-6 font-urbanist font-light ml-auto">
-          <li><a href="#home" className="hover:text-yellow-500 text-white">Home</a></li>
-          <li><a href="#about" className="hover:text-yellow-500 text-white">About</a></li>
-          <li><a href="#services" className="hover:text-yellow-500 text-white">Services</a></li>
-          <li><a href="#portfolio" className="hover:text-yellow-500 text-white">Projects</a></li>
+          {/* <li><a href="#home" className=" text-white">Home</a></li>
+          <li><a href="#about" className=" text-white">About</a></li>
+          <li><a href="#services" className=" text-white">Services</a></li>
+          <li><a href="#portfolio" className=" text-white">Projects</a></li>
           <li>
-            <div className="inline-flex items-center gap-1 px-2 py-0.5 bg-yellow-500 rounded-full group hover:bg-white transition-colors">
-              <a href="#contact" className="text-white font-bold animate-pulse group-hover:text-yellow-500">Connect</a>
-              <BsFillTelephoneFill className="w-4 h-4 text-gray-400 scale-x-[-1] group-hover:text-gray-400" />
+            <div className="inline-flex items-center gap-1 px-2 py-0.5 rounded-full group hover:bg-white transition-colors">
+              <a href="#contact" className="text-white font-bold animate-pulse group-hover:text-cyan-500">Connect</a>
+              <BsFillTelephoneFill className="w-4 h-4 text-gray-400 scale-x-[-1] group-hover:text-cyan-400" />
             </div>
-          </li>
+          </li> */}
         </ul>
       </div>
       {isOpen && (
